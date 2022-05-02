44ef38ab892872ee3e91fb22d824e84b505b5699
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-03 01:49:15 +0200

    [CanRxThread] working

--- ChibiOS/CanRxThread.hpp
+++ ChibiOS/CanRxThread.hpp
@@ -13,26 +13,31 @@ struct CanardSubscribeMetadata {
 
 typedef uint8_t listenerCount_t;
 
-constexpr uint16_t CAN_RX_WA = 0x100;
+constexpr uint16_t CAN_RX_WA = 0x400;
 constexpr listenerCount_t  CAN_RX_MAX_LISTENERS = 8;
-constexpr uint8_t          CAN_RX_MAX_SUBSCRIPTION = 16;
+constexpr uint8_t          CAN_RX_MAX_SUBSCRIPTION = 4;
 
 class CanRxThread : public chibios_rt::BaseStaticThread<CAN_RX_WA> {
 public:
-    CanRxThread();
+    CanRxThread(CanardInstance* instance);
 
-    inline void setCanardInstance(CanardInstance* instance) { this->m_instance = instance;};
-
-    bool subscribe(CanListener *listener, CanardRxSubscription* subscription, CanardSubscribeMetadata metadata);
+    bool subscribe(CanListener *listener,
+                   CanardTransferKind transfer_kind,
+                   CanardPortID port_id,
+                   size_t extent);
 
 private:
     void main() override;
     CanListener *   m_listeners[CAN_RX_MAX_LISTENERS];
+    uint8_t         m_listenerSubCnt[CAN_RX_MAX_LISTENERS];
+    CanardRxSubscription * m_subLUT[CAN_RX_MAX_LISTENERS][CAN_RX_MAX_SUBSCRIPTION];
     listenerCount_t m_listenersCount;
     CanardInstance * m_instance;
-    CanardRxSubscription* m_sub[CAN_RX_MAX_SUBSCRIPTION];
+    CanardRxSubscription m_sub[CAN_RX_MAX_SUBSCRIPTION];
     uint8_t m_subCnt;
 
-    bool registerListener(CanListener * listener);
+    bool registerListener(CanListener *listener, uint8_t * idx);
+
+    void notifyListeners(CanardRxSubscription * sub, CanardRxTransfer* transfer);
 };
 
