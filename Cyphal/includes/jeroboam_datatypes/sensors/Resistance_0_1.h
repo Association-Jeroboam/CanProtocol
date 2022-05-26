1f212d677f8b546937d32a8a06c9e76683933631
Thomas Jeantet <tj.thomas.jeantet@gmail.com>
2022-05-26 03:26:21 +0200

    Fixed resistance naming

--- Cyphal/includes/jeroboam_datatypes/sensors/Resistance_0_1.h
+++ Cyphal/includes/jeroboam_datatypes/sensors/Resistance_0_1.h
@@ -8,11 +8,11 @@
 // are named with an underscore at the end, like foo_bar_().
 //
 // Generator:     nunavut-1.8.0 (serialization was enabled)
-// Source file:   /home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan
-// Generated at:  2022-05-16 20:53:59.233549 UTC
+// Source file:   /home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan
+// Generated at:  2022-05-26 01:24:40.792408 UTC
 // Is deprecated: no
 // Fixed port-ID: None
-// Full name:     jeroboam_datatypes.sensors.resistance
+// Full name:     jeroboam_datatypes.sensors.Resistance
 // Version:       0.1
 //
 // Platform
@@ -40,19 +40,19 @@
 #include <stdlib.h>
 
 static_assert( NUNAVUT_SUPPORT_LANGUAGE_OPTION_TARGET_ENDIANNESS == 434322821,
-              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
+              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 static_assert( NUNAVUT_SUPPORT_LANGUAGE_OPTION_OMIT_FLOAT_SERIALIZATION_SUPPORT == 0,
-              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
+              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 static_assert( NUNAVUT_SUPPORT_LANGUAGE_OPTION_ENABLE_SERIALIZATION_ASSERTS == 0,
-              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
+              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 static_assert( NUNAVUT_SUPPORT_LANGUAGE_OPTION_ENABLE_OVERRIDE_VARIABLE_ARRAY_CAPACITY == 0,
-              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
+              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 static_assert( NUNAVUT_SUPPORT_LANGUAGE_OPTION_CAST_FORMAT == 2368206204,
-              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
+              "/home/thomas/git/ActionBoardFirmware/firmware/libs/CanProtocol/Cyphal/jeroboam_datatypes/sensors/Resistance.0.1.uavcan is trying to use a serialization library that was compiled with "
               "different language options. This is dangerous and therefore not allowed." );
 
 #ifdef __cplusplus
@@ -60,10 +60,10 @@ extern "C" {
 #endif
 
 /// This type does not have a fixed port-ID. See https://forum.opencyphal.org/t/choosing-message-and-service-ids/889
-#define jeroboam_datatypes_sensors_resistance_0_1_HAS_FIXED_PORT_ID_ false
+#define jeroboam_datatypes_sensors_Resistance_0_1_HAS_FIXED_PORT_ID_ false
 
-#define jeroboam_datatypes_sensors_resistance_0_1_FULL_NAME_             "jeroboam_datatypes.sensors.resistance"
-#define jeroboam_datatypes_sensors_resistance_0_1_FULL_NAME_AND_VERSION_ "jeroboam_datatypes.sensors.resistance.0.1"
+#define jeroboam_datatypes_sensors_Resistance_0_1_FULL_NAME_             "jeroboam_datatypes.sensors.Resistance"
+#define jeroboam_datatypes_sensors_Resistance_0_1_FULL_NAME_AND_VERSION_ "jeroboam_datatypes.sensors.Resistance.0.1"
 
 /// Extent is the minimum amount of memory required to hold any serialized representation of any compatible
 /// version of the data type; or, on other words, it is the the maximum possible size of received objects of this type.
@@ -72,16 +72,16 @@ extern "C" {
 /// When allocating a serialization (TX) buffer, it is safe to use the size of the largest serialized representation
 /// instead of the extent because it provides a tighter bound of the object size; it is safe because the concrete type
 /// is always known during serialization (unlike deserialization). If not sure, use extent everywhere.
-#define jeroboam_datatypes_sensors_resistance_0_1_EXTENT_BYTES_                    4UL
-#define jeroboam_datatypes_sensors_resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_ 4UL
-static_assert(jeroboam_datatypes_sensors_resistance_0_1_EXTENT_BYTES_ >= jeroboam_datatypes_sensors_resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_,
+#define jeroboam_datatypes_sensors_Resistance_0_1_EXTENT_BYTES_                    4UL
+#define jeroboam_datatypes_sensors_Resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_ 4UL
+static_assert(jeroboam_datatypes_sensors_Resistance_0_1_EXTENT_BYTES_ >= jeroboam_datatypes_sensors_Resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_,
               "Internal constraint violation");
 
 typedef struct
 {
     /// uavcan.primitive.scalar.Real32.1.0 value
     uavcan_primitive_scalar_Real32_1_0 value;
-} jeroboam_datatypes_sensors_resistance_0_1;
+} jeroboam_datatypes_sensors_Resistance_0_1;
 
 /// Serialize an instance into the provided buffer.
 /// The lifetime of the resulting serialized representation is independent of the original instance.
@@ -91,7 +91,7 @@ typedef struct
 /// @param obj      The object to serialize.
 ///
 /// @param buffer   The destination buffer. There are no alignment requirements.
-///                 @see jeroboam_datatypes_sensors_resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_
+///                 @see jeroboam_datatypes_sensors_Resistance_0_1_SERIALIZATION_BUFFER_SIZE_BYTES_
 ///
 /// @param inout_buffer_size_bytes  When calling, this is a pointer to the size of the buffer in bytes.
 ///                                 Upon return this value will be updated with the size of the constructed serialized
@@ -99,8 +99,8 @@ typedef struct
 ///                                 layer. In case of error this value is undefined.
 ///
 /// @returns Negative on error, zero on success.
-static inline int8_t jeroboam_datatypes_sensors_resistance_0_1_serialize_(
-    const jeroboam_datatypes_sensors_resistance_0_1* const obj, uint8_t* const buffer,  size_t* const inout_buffer_size_bytes)
+static inline int8_t jeroboam_datatypes_sensors_Resistance_0_1_serialize_(
+    const jeroboam_datatypes_sensors_Resistance_0_1* const obj, uint8_t* const buffer,  size_t* const inout_buffer_size_bytes)
 {
     if ((obj == NULL) || (buffer == NULL) || (inout_buffer_size_bytes == NULL))
     {
@@ -173,8 +173,8 @@ static inline int8_t jeroboam_datatypes_sensors_resistance_0_1_serialize_(
 ///                                 was activated. In case of error this value is undefined.
 ///
 /// @returns Negative on error, zero on success.
-static inline int8_t jeroboam_datatypes_sensors_resistance_0_1_deserialize_(
-    jeroboam_datatypes_sensors_resistance_0_1* const out_obj, const uint8_t* buffer, size_t* const inout_buffer_size_bytes)
+static inline int8_t jeroboam_datatypes_sensors_Resistance_0_1_deserialize_(
+    jeroboam_datatypes_sensors_Resistance_0_1* const out_obj, const uint8_t* buffer, size_t* const inout_buffer_size_bytes)
 {
     if ((out_obj == NULL) || (inout_buffer_size_bytes == NULL) || ((buffer == NULL) && (0 != *inout_buffer_size_bytes)))
     {
@@ -219,13 +219,13 @@ static inline int8_t jeroboam_datatypes_sensors_resistance_0_1_deserialize_(
 /// This function intentionally leaves inactive elements uninitialized; for example, members of a variable-length
 /// array beyond its length are left uninitialized; aliased union memory that is not used by the first union field
 /// is left uninitialized, etc. If full zero-initialization is desired, just use memset(&obj, 0, sizeof(obj)).
-static inline void jeroboam_datatypes_sensors_resistance_0_1_initialize_(jeroboam_datatypes_sensors_resistance_0_1* const out_obj)
+static inline void jeroboam_datatypes_sensors_Resistance_0_1_initialize_(jeroboam_datatypes_sensors_Resistance_0_1* const out_obj)
 {
     if (out_obj != NULL)
     {
         size_t size_bytes = 0;
         const uint8_t buf = 0;
-        const int8_t err = jeroboam_datatypes_sensors_resistance_0_1_deserialize_(out_obj, &buf, &size_bytes);
+        const int8_t err = jeroboam_datatypes_sensors_Resistance_0_1_deserialize_(out_obj, &buf, &size_bytes);
 
         (void) err;
     }
